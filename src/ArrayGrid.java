036d981f75ab54b0c7a671cf91fdf5b4d74ca32c
ShendongChang <csd594575692@gmail.com>
2019-07-31 18:10:38 +1000

    Initial commit

---
+++ src/ArrayGrid.java
@@ -0,0 +1,21 @@
+/**
+ * A 2D grid implemented as an array.
+ * Each (x,y) coordinate can hold a single item of type <T>.
+ *
+ * @param <T> The type of element held in the data structure
+ */
+public class ArrayGrid<T> implements Grid<T> {
+
+	/**
+	 * Constructs a new ArrayGrid object with a given width and height.
+	 *
+	 * @param width The width of the grid
+	 * @param height The height of the grid
+     * @throws IllegalArgumentException If the width or height is less than or equal to zero
+	 */
+	public ArrayGrid(int width, int height) throws IllegalArgumentException {
+	    // TODO: implement the constructor
+	}
+
+	// TODO: implement all of Grid's methods here
+}
