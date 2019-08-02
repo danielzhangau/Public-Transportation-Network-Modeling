ef7381e39b3ed6c4b65b1e923e08d5070f049ee7
shendongchang <53511450+shendongchang@users.noreply.github.com>
2019-08-02 21:33:24 +1000

    Update ArrayGrid.java

--- src/ArrayGrid.java
+++ src/ArrayGrid.java
@@ -1,21 +1,88 @@
-/**
- * A 2D grid implemented as an array.
- * Each (x,y) coordinate can hold a single item of type <T>.
- *
- * @param <T> The type of element held in the data structure
- */
+
 public class ArrayGrid<T> implements Grid<T> {
 
-	/**
-	 * Constructs a new ArrayGrid object with a given width and height.
-	 *
-	 * @param width The width of the grid
-	 * @param height The height of the grid
-     * @throws IllegalArgumentException If the width or height is less than or equal to zero
-	 */
+	public T[][] arry;
+	public T[][] newArry;
+	public int x_axis;
+	public int y_axis;
+
+
 	public ArrayGrid(int width, int height) throws IllegalArgumentException {
-	    // TODO: implement the constructor
+
+		if (width <= 0 || height <= 0) {
+			throw new IllegalArgumentException("Failed,width or height equal or less than 0");
+		}
+		arry = (T[][]) new Object[width][height];
+		x_axis = width;
+		y_axis = height;
+
 	}
 
-	// TODO: implement all of Grid's methods here
+
+	@Override
+	public void add(int x, int y, T element) throws IllegalArgumentException {
+		if (x < 0 || y < 0 || x >= x_axis || y >= y_axis) {
+			throw new IllegalArgumentException("Failed,add x or y equal or less than 0, or over axis");
+		} else {
+			arry[x][y] = element;
+		}
+
+	}
+
+	@Override
+	public T get(int x, int y) throws IndexOutOfBoundsException {
+
+		System.out.println(String.format("x:%d y:%d element:%s", x, y, arry[x][y]));
+		if (x >= arry.length || y >= arry[0].length) {
+			throw new IllegalArgumentException("Failed, get x or y equal or less than 0, or over axis");
+		} else if (arry[x][y] == null) {
+			//System.out.println("null");
+			return null;
+		} else {
+			return arry[x][y];
+		}
+	}
+
+	@Override
+	public boolean remove(int x, int y) throws IndexOutOfBoundsException {
+		if (x < 0 || y < 0 || x >= x_axis || y >= y_axis) {
+			return false;
+		} else if (arry[x][y] == null) {
+			return false;
+		} else {
+			arry[x][y] = null;
+			return true;
+		}
+
+	}
+
+	@Override
+	public void clear() {
+		int x = 0, y = 0;
+		while (x < x_axis) {
+			while (y < y_axis) {
+				arry[x][y] = null;
+				y++;
+			}
+			x++;
+		}
+	}
+
+	@Override
+	public void resize(int newWidth, int newHeight) throws IllegalArgumentException {
+		if (-newWidth > x_axis || -newHeight > y_axis){
+			throw new IllegalArgumentException("axis change decreased too much, the axis won't exist");
+		}
+		newArry = (T[][]) new Object[newWidth][newHeight];
+		for (int x = 0; x < x_axis; x++){
+			for (int y = 0; y < y_axis; y++){
+				if (arry[x][y] != null){
+					newArry[x][y] = arry[x][y];
+				}
+			}
+		}
+		arry = newArry;
+	}
 }
+
+
